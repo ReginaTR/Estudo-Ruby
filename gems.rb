2f5f4be4e23e5437cc8d793c21d655533786051c
Regina Tarquinio Reis <regininhar@hotmail.com>
2024-04-29 19:57:03 -0300

    gems
    Estudo de gems cpf no ruby

---
+++ gems.rb
@@ -0,0 +1,14 @@
+#Siga a documentação da gem cpf_cnpj para criar um programa que recebe como entrada um numero de cpf e em um método verifique se esse numero é valido.
+
+require 'cpf_cnpj'
+
+
+puts "Digite o cpf a ser validado:"
+    cpf = gets.chomp.to_i
+
+if CPF.valid?(cpf)
+   puts 'CPF válido!'
+else 
+    puts 'CPF inválido!'   
+end 
+
